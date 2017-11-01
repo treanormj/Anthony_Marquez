a60aa785a19d935b9b167cbb42fb4bf1fc949199
treanormj <treanormj@gmail.com>
2017-11-01 11:41:44 -0400

    new not working

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -5,12 +5,12 @@ class ReflectionsController < ApplicationController
   end
 
   def create
-    @reflection = Reflection.new(reflection_path)
+    @reflection = Reflection.new(reflection_params)
     # @answer = Answer.new(answer_params)
     # @answer.question = @question
   # if the question is saved correctly, it will send a notice. If it is not, it just goes back to the form page.
     if @reflection.save
-      redirect_to comment_path(@reflection), notice: "Reflection was successfully created."
+      redirect_to reflection_path(@reflection), notice: "Reflection was successfully created."
     else
       render :new
     end
@@ -22,8 +22,6 @@ class ReflectionsController < ApplicationController
 
   def show
     @reflection = Reflection.find(params[:id])
-    # @answers = @question.answers
-    # @answer = Answer.new
   end
 
   def destroy
@@ -31,6 +29,7 @@ class ReflectionsController < ApplicationController
     @reflection.destroy
     redirect_to reflections_url, notice: 'Comment was successfully deleted.'
   end
+
 end
   # this reduces any junk in our submissions. It keeps it with title and description. They could mess things up with curl requests.
     # private
